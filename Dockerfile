a7688b53db45c4fe660a1f2da9b7a10e5d2057af
Chong Wang <chong.w.wang@daimler.com>
2020-04-19 14:41:12 +0800

    test in edge node

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM openjdk:11.0.7-jre-slim
 
 USER root
-COPY ./krb5.conf /app/krb5.conf
+COPY ./krb5.conf /home/bdp_admin_s/HiveTest/krb5.conf
 COPY ./bdp_admin_s.keytab /app/bdp_admin_s.keytab
 COPY ./bdp_otrdwh_s.keytab /app/bdp_otrdwh_s.keytab
 COPY ./HiveJdbc/build/libs/HiveJdbc-1-all.jar /app/HiveJdbc-1-all.jar
--- src/main/java/com/hive/Activity_History.java
+++ src/main/java/com/hive/Activity_History.java
@@ -11,9 +11,9 @@ import org.slf4j.LoggerFactory;
 public class Activity_History {
 
 
-    public static void test(String userName) throws SQLException {
+    public static void testWithAdmin() throws SQLException {
         final Logger logger = LoggerFactory.getLogger(Activity_History.class);
-        Connection conn = JdbcHelper.getConnect(userName);
+        Connection conn = JdbcHelper.getConnect("bdp_admin_s");
         String sql = "SELECT * FROM rawdata_cdm.rw_cust_cdm_accnt LIMIT 10" ;
 
         PreparedStatement stmt = conn.prepareStatement(sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
@@ -27,6 +27,27 @@ public class Activity_History {
             }
         }
         System.out.println("send A_history json success!");
+        rs.close();
+        stmt.close();
+        conn.close();
+    }
+
+    public static void testWithDWH() throws SQLException {
+        final Logger logger = LoggerFactory.getLogger(Activity_History.class);
+        Connection conn = JdbcHelper.getConnect("bdp_otrdwh_s");
+        String sql = "select province, dms_code from revr_bmbs_otr.td_car_sales_dealer LIMIT 10" ;
+
+        PreparedStatement stmt = conn.prepareStatement(sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+        stmt.setFetchSize(1024);
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            try {
+                System.out.println(rs.getString("province") + "    " + rs.getString("dms_code"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("got td_car_sales_dealer success!");
         rs.close();
         stmt.close();
         conn.close();
